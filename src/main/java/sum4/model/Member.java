cac72e4e40816337d4b0cd89b57d2f6db4325d52
nkmorjhb025 <nkmorjhb025@student.wethinkcode.co.za>
2026-06-10 14:43:16 +0200

    added member class

---
+++ src/main/java/sum4/model/Member.java
@@ -0,0 +1,11 @@
+package sum4.model;
+
+public class Member {
+    // TODO: Declare private immutable fields
+
+    public Member(String memberId, String fullName) {
+        // TODO: Implement constructor
+    }
+
+    // TODO: Implement getters and toString()
+}
