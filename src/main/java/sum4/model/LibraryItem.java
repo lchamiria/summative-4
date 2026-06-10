315701ec034904b5f3011d646644fe32d8b1faa6
nkmorjhb025 <nkmorjhb025@student.wethinkcode.co.za>
2026-06-10 14:37:24 +0200

    added library item  class

---
+++ src/main/java/sum4/model/LibraryItem.java
@@ -0,0 +1,11 @@
+package sum4.model;
+
+public abstract class LibraryItem {
+    // TODO: Declare private fields
+
+    // TODO: Implement constructor
+
+    // TODO: Implement getters and setters
+
+    // TODO: Declare abstract calculateLateFee method
+}
